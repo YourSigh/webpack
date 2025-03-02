92b9ee46a58e36c1ac86fdaa6bf562cde1e3e67f
绿桶 <1325285734@qq.com>
2025-03-02 10:33:21 +0800

    feat:提取公共配置文件

---
+++ config/webpack.dev.js
@@ -0,0 +1,12 @@
+// 引入webpack-merge
+const { merge } = require("webpack-merge");
+// 引入公共配置
+const common = require("./webpack.common.js");
+// 第一个参数是公共配置 第二个参数是环境里的配置
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+});
